4752998a43e486ba71f59d2a9610ea482631ac9a
RisqiNurRohmat <risqinurrohmat@gmai.com>
2023-05-11 16:25:01 +0700

    Mengerjakan UTS

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
-// test
-// Test2
-// test3
-// test5
+        Mhs mhs = new Mhs ("Risqi Nur Rohmat", "G.111.20.0011", 6);
+
+        // mhs.setNama("Risqi Nur Rohmat");
+        // mhs.setNim("G.111.20.0011");
+        // mhs.setSmt(6);
+
+        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        // System.out.println("Username: " + mhs.getUsername());
+        // System.out.println("Password: " + mhs.getPassword());
+
+
     }
 }
--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,41 @@
 package pengguna;
 
-class Mhs {
-    Mhs () {
+ 
+    public class Mhs {
+        private String Nama;
+        private String Nim ;
+        private int Smt;
+    
+        public Mhs(String Nama, String Nim, int Smt){
+            this.Nama = Nama;
+            this.Nim = Nim;
+            this.Smt = Smt;
+}
+public Mhs(){
+
+}
+
+public void Nama(String Nama) {
+    this.Nama = Nama;
+}
+public String getNama(){
+    return this.Nama;
+}
+
+
+public void setNim(String Nim){
+    this.Nim = Nim;
+}
+public String getNim(){
+    return this.Nim;
+}
+
+
+public void setSmt(int Smt){
+    this.Smt = Smt;
+}
+public int getSmt(){
+    return this.Smt;
+}
 
     }
-}
